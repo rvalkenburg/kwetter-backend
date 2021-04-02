11427c2a50c0929efbe6bc13b809392a80de3dd4
Roger Valkenburg <I412512@fhict.nl>
2021-04-02 16:04:00 +0200

    Added kweetservice

---
+++ src/Services/KweetService/Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kwetter.Services.KweetService.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/KweetService/Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kwetter.Services.KweetService.Persistence
+{
+    public class Class1
+    {
+    }
+}
