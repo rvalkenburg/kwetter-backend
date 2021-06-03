a3c0f0cb86f8de4b32fedc195be350045d75c509
Roger Valkenburg <I412512@fhict.nl>
2021-06-03 19:09:39 +0200

    Updated gateway to support kubernetes

--- src/Gateway/Startup.cs
+++ src/Gateway/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Kubernetes;
 
 namespace Kwetter.Gateway
 {
@@ -13,16 +14,14 @@ namespace Kwetter.Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();  
+            services.AddOcelot()
+                .AddKubernetes();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseRouting();
 
