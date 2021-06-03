7229fa6b633be83a765985b7a7320813570afdb3
rogervalkenburg@gmail.com <rogervalkenburg@gmail.com>
2021-06-03 11:12:25 +0200

    removed seeder

--- src/Services/ProfileService/Application/DependencyInjection.cs
+++ src/Services/ProfileService/Application/DependencyInjection.cs
@@ -43,34 +43,7 @@ namespace Kwetter.Services.ProfileService.Application
             
             services.AddHostedService(sp => consumer);
 
-            Seed(serviceProvider);
-            
             return services;
-        }
-
-        private async static void Seed(ServiceProvider serviceProvider)
-        {
-            ICreateUserHandler context = serviceProvider.GetRequiredService<ICreateUserHandler>();
-            List<string> profiles = new List<string>();
-            
-            for (int i = 1; i < 10; i++)
-            {
-                Random rnd = new Random();
-                UserEvent profile = new UserEvent();
-                profile.Id = new Guid();
-                profile.Avatar = $"http://placehold.it/120x120&text=image{i}";
-                profile.DisplayName = $"Test user {i}";
-                profile.GoogleId = rnd.Next(52).ToString(); 
-                profile.Email = "test@google.com";
-                profile.Description = "Test";
-                profile.DateOfCreation = DateTime.Now;
-                profiles.Add(JsonConvert.SerializeObject(profile));
-            }
-            
-            foreach (var profile in profiles)
-            {
-                await context.Consume(profile);
-            }
         }
     }
 }
