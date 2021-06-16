1623535dc68021113c19419d1918f81f6fce1d13
Roger Valkenburg <I412512@fhict.nl>
2021-06-16 11:45:18 +0200

    Set cors policy profile service

--- src/Services/ProfileService/Rest/Startup.cs
+++ src/Services/ProfileService/Rest/Startup.cs
@@ -74,7 +74,9 @@ namespace Kwetter.Services.ProfileService.Rest
             }
 
             app.UseRouting();
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(x =>
+                x.WithOrigins("http://20.82.87.48:80").AllowAnyMethod()
+                    .WithHeaders("authorization", "accept", "content-type", "origin"));
             app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
